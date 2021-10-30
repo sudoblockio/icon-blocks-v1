655691318bcd7289e80d64b438979eab9f9ba6a2
Pranav <pranav@geometrylabs.io>
2021-10-29 19:02:04 -0700

    add block number range integration test

--- tests/blocks_test.go
+++ tests/blocks_test.go
@@ -55,7 +55,7 @@ func TestBlocksEndpointListLimitSkip(t *testing.T) {
 		blocksServiceRestPrefx = "/api/v1"
 	}
 
-	resp, err := http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?limit=100&skip=100")
+	resp, err := http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?limit=20&skip=20")
 	assert.Equal(nil, err)
 	assert.Equal(200, resp.StatusCode)
 
@@ -104,10 +104,66 @@ func TestBlocksEndpointListNumber(t *testing.T) {
 	assert.Equal(nil, err)
 	assert.NotEqual(0, len(bodyMap))
 
-	blockNumber := uint64(bodyMap[0].(map[string]interface{})["number"].(float64))
+	// Get testable number
+	blockNumber := strconv.FormatUint(uint64(bodyMap[0].(map[string]interface{})["number"].(float64)), 10)
 
 	// Test number
-	resp, err = http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?number=" + strconv.FormatUint(blockNumber, 10))
+	resp, err = http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?number=" + blockNumber)
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	// Test headers
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
+
+	bytes, err = ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap = make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+}
+
+// List number range test
+func TestBlocksEndpointListNumberRange(t *testing.T) {
+	assert := assert.New(t)
+
+	blocksServiceURL := os.Getenv("BLOCKS_SERVICE_URL")
+	if blocksServiceURL == "" {
+		blocksServiceURL = "http://localhost:8000"
+	}
+	blocksServiceRestPrefx := os.Getenv("BLOCKS_SERVICE_REST_PREFIX")
+	if blocksServiceRestPrefx == "" {
+		blocksServiceRestPrefx = "/api/v1"
+	}
+
+	// Get latest block
+	resp, err := http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?limit=100")
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	// Test headers
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap := make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+
+	// Get testable number
+	// NOTE list is DESC order
+	blockNumberStart := strconv.FormatUint(uint64(bodyMap[len(bodyMap)-1].(map[string]interface{})["number"].(float64)), 10)
+	blockNumberEnd := strconv.FormatUint(uint64(bodyMap[0].(map[string]interface{})["number"].(float64)), 10)
+
+	// Test number
+	resp, err = http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?start_number=" + blockNumberStart + "&end_number=" + blockNumberEnd)
 	assert.Equal(nil, err)
 	assert.Equal(200, resp.StatusCode)
 
