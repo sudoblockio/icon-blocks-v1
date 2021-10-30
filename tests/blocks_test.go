6bd6038876df62aa59b7e1d10b784f7c5e878268
Pranav <pranav@geometrylabs.io>
2021-10-29 19:58:06 -0700

    add block hash integration test

--- tests/blocks_test.go
+++ tests/blocks_test.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -164,6 +165,61 @@ func TestBlocksEndpointListNumberRange(t *testing.T) {
 
 	// Test number
 	resp, err = http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?start_number=" + blockNumberStart + "&end_number=" + blockNumberEnd)
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	// Test headers
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
+
+	bytes, err = ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap = make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+}
+
+// List hash test
+func TestBlocksEndpointListHash(t *testing.T) {
+	assert := assert.New(t)
+
+	blocksServiceURL := os.Getenv("BLOCKS_SERVICE_URL")
+	if blocksServiceURL == "" {
+		blocksServiceURL = "http://localhost:8000"
+	}
+	blocksServiceRestPrefx := os.Getenv("BLOCKS_SERVICE_REST_PREFIX")
+	if blocksServiceRestPrefx == "" {
+		blocksServiceRestPrefx = "/api/v1"
+	}
+
+	// Get latest block
+	resp, err := http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?limit=1")
+	assert.Equal(nil, err)
+	assert.Equal(200, resp.StatusCode)
+
+	defer resp.Body.Close()
+
+	// Test headers
+	assert.NotEqual("0", resp.Header.Get("X-TOTAL-COUNT"))
+
+	bytes, err := ioutil.ReadAll(resp.Body)
+	assert.Equal(nil, err)
+
+	bodyMap := make([]interface{}, 0)
+	err = json.Unmarshal(bytes, &bodyMap)
+	assert.Equal(nil, err)
+	assert.NotEqual(0, len(bodyMap))
+
+	// Get testable number
+	// NOTE list is DESC order
+	blockHash := bodyMap[0].(map[string]interface{})["hash"].(string)
+	fmt.Println(blockHash)
+
+	// Test number
+	resp, err = http.Get(blocksServiceURL + blocksServiceRestPrefx + "/blocks?hash=" + blockHash)
 	assert.Equal(nil, err)
 	assert.Equal(200, resp.StatusCode)
 
